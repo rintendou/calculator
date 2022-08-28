5a26bc4d17dc8ea1255ebf9c47b2f29236273213
Danny Chen <dannychen8102@gmail.com>
2022-08-28 12:05:12 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const display = document.querySelector(".display");
+
+const numbers = document.querySelectorAll('[data-number]');
+const operations = document.querySelectorAll('[data-operation]');
+
